37b6bbaa096ff32dedb0d43c91995a98b7dc816c
Ignis010 <100122857+Ignis010@users.noreply.github.com>
2022-05-26 19:05:52 +0900

    Add files via upload

---
+++ src/components/elements/MyButton.vue
@@ -0,0 +1,23 @@
+<template>
+ <button class="button">
+   <slot></slot>
+ </button>
+</template>
+
+<script>
+export default {
+  name: "MyButton"
+}
+</script>
+
+<style scoped>
+.button {
+  width: 55%;
+  height: 10%;
+  border-radius: 10px;
+  border: 2px solid #336699;
+  margin-top: 5%;
+  background-color: #99CCFF;
+  font-size: 14pt;
+}
+</style>
---
+++ src/components/elements/MyInput.vue
@@ -0,0 +1,19 @@
+<template>
+    <input class="input">
+</template>
+
+<script>
+export default {
+  name: "MyInput"
+}
+</script>
+
+<style scoped>
+ .input {
+   width: 70%;
+   height: 7%;
+   border-radius: 10px;
+   box-shadow: inset 1px  1px 4px black;
+   margin-top: 5%;
+ }
+</style>
